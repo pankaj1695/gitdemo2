89133eba60129e9a43dfcb34263f3bbacb6ae379
Pankaj <pankaj1695kumar@gmail.com>
2025-01-29 21:44:20 +0530

    commit 2

--- gitdemo1/src/com/test1/Java8Opererion.java
+++ gitdemo1/src/com/test1/Java8Opererion.java
@@ -2,6 +2,8 @@ package com.test1;
 
 import java.security.DomainCombiner;
 import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Java8Opererion {
@@ -11,5 +13,10 @@ public static void main(String[] args) {
 	System.out.println(sortstr);
 	String remstr=Arrays.stream(str.split("")).distinct().collect(Collectors.joining());
 	System.out.println(remstr);
+	
+	Map<String,Long> map1=Arrays.stream(str.split("")).collect(Collectors.groupingBy(Function.identity()
+			,Collectors.counting()));
+	
+	System.out.println(map1);
 }
 }
